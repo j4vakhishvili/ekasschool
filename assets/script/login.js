37c94912d38432b59e40753c66d052e5c42bd33c
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2021-11-23 09:40:51 +0400

    Update login.js

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,11 +1,10 @@
-var logo = document.getElementsByClassName("logo");
-
 function validate(){
     var key = document.getElementById("key").value;
     var keyIDone = "36Sd10Wy";
     var keyIDtwo = "ekaboss";
+    var keyIDthree = "admin";
 
-    if(key == keyIDone || key == keyIDtwo){
+    if(key == keyIDone || key == keyIDtwo || key == keyIDthree){
         window.location.href='main.html';
         return false;
     } else{
